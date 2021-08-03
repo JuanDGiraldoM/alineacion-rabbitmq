396d8331a5d7f879106fd1ba339bc1ad402803f5
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2021-08-03 10:04:23 -0500

    Request/Reply

--- src/main/java/co/com/bancolombia/poc/rabbitmq/SampleSender.java
+++ src/main/java/co/com/bancolombia/poc/rabbitmq/SampleSender.java
@@ -4,7 +4,6 @@ import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Delivery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
