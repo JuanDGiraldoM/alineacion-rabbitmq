acdc65d8e55af6365378a4e5d8d31ac489233d8d
JuanDGiraldoM <jgmarin@bancolombia.com.co>
2021-08-03 09:32:32 -0500

    Request

--- src/main/java/co/com/bancolombia/poc/rabbitmq/SampleSender.java
+++ src/main/java/co/com/bancolombia/poc/rabbitmq/SampleSender.java
@@ -1,5 +1,6 @@
 package co.com.bancolombia.poc.rabbitmq;
 
+import com.rabbitmq.client.Delivery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,7 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 import reactor.rabbitmq.OutboundMessage;
+import reactor.rabbitmq.RpcClient;
 import reactor.rabbitmq.Sender;
+
+import java.util.UUID;
+import java.util.function.Supplier;
 
 @RestController
 @RequestMapping("/rabbitmq")
@@ -26,6 +31,23 @@ public class SampleSender {
         LOGGER.info("Sending Message {}", message.getMessage());
         return sender.send(createMessage(message))
                 .thenReturn("OK");
+    }
+
+    @PostMapping("/sendto")
+    private Mono<String> requestReply(@RequestBody Message message) {
+        LOGGER.info("Sending Message {}", message.getMessage());
+
+        // TODO: 3. Create request for reply
+        // Correlation ID
+        Supplier<String> correlationId = () -> UUID.randomUUID().toString();
+        // Create RPC Client with correlation ID param
+        RpcClient client = sender.rpcClient(message.getExchange(), message.getRoutingKey(), correlationId);
+        // Send body through rpc request
+        RpcClient.RpcRequest request = new RpcClient.RpcRequest(message.getMessage().getBytes());
+        // Get the response
+        Mono<Delivery> response = client.rpc(Mono.just(request));
+        // Return the delivery response
+        return response.map(delivery -> new String(delivery.getBody()));
     }
 
     private Mono<OutboundMessage> createMessage(Message message) {
